ad0dc3e022b5a66b0c6bbb6bf517897b408dcb8a
Ben Potter (Admin) <ben@coder.com>
2021-06-10 15:17:31 +0000

    styles

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 from flask import Flask, render_template
+import glob, os
 app = Flask(__name__)
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    people_folder = dir_path + "/templates/people"
+    people_list = {}
+    for file in os.listdir(people_folder):
+        if file.endswith(".html") or file.endswith(".jinja"):
+            # get their name with some string magic
+            route = file.replace(".html", "")
+            name = route.replace("_", " ").title()
+            people_list[name] = route
+    return render_template('index.html', people=people_list)
+    
+# Custom routes
+@app.route('/people/ben_potter')
+def ben(name="ben_potter"):
+    # some special logic here
+    print ("ben's special page")
+    return render_template("people/{}.html".format(name), name=name)
 
-@app.route('/people')
-def people():
-    return render_template('list_people.html')
-
+# Basic routes with no special logic
 @app.route('/people/<name>')
 def person(name=None):
-    return render_template('person.html', name=name)
+    return render_template("people/{}.html".format(name), name=name)
